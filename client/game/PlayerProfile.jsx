cad803dc87febc9d7852902391f14637c3e9ffeb
Jaeyoon Song <jyo3on@gmail.com>
2021-07-27 15:59:09 -0400

    remove expected rewards

--- client/game/PlayerProfile.jsx
+++ client/game/PlayerProfile.jsx
@@ -28,14 +28,6 @@ export default class PlayerProfile extends React.Component {
         <b>Total</b>:<br/>
         <span>{score1 + score2} stories submitted</span><br/><br/>
 
-        <b>Expected reward</b>:<br/>
-        {score1 == 0 ? 
-          <span>$0 <br/><small>*You need to write at least 1 story in each round</small></span>
-        :
-          <span>$1 ~ ${1 + 2 * (score1 + score2)}</span>
-        }
-        <br/>
-        <small>{score1 != 0 && score2 == 0? "*Only if you write at least 1 story in round 2" : ""}</small>
       </div>
 
     );
